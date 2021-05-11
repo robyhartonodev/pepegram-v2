e7c7f34de8fc3d8f69dc17ca5104850781f29bb0
rhrt04 <roby.hartono@ges.thm.de>
2021-05-11 18:54:39 +0200

    Updated spacing in router.js

--- resources/js/router/routes.js
+++ resources/js/router/routes.js
@@ -13,6 +13,7 @@ export default [
   { path: '/email/resend', name: 'verification.resend', component: page('auth/verification/resend.vue') },
 
   { path: '/home/:id', name: 'home', component: page('home.vue') },
+
   {
     path: '/settings',
     component: page('settings/index.vue'),
